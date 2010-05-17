c8f339172bae4b3e0d05db4cad340d997b9a617e
flashxml <flashxml@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-17 07:28:27 +0000

    git-svn-id: https://plugins.svn.wordpress.org/3d-wall-fx/trunk@242209 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ 3d-wall-fx.php
@@ -0,0 +1,81 @@
+<?php
+/*
+Plugin Name: 3D Wall FX
+Plugin URI: http://www.flashxml.net/3d-wall.html
+Description: One of the most advanced 3D Wall on the web. Completely XML customizable, without using Flash. And it's free!
+Version: 0.1.1
+Author: FlashXML.net
+Author URI: http://www.flashxml.net/
+License: GPL2
+*/
+
+	$wallfx_params = array(
+		'count'	=> 0, // number of 3D Wall FX embeds
+	);
+
+	function wallfx_get_embed_code($wallfx_attributes) {
+		$plugin_dir = basename(dirname(__FILE__));
+		global $wallfx_params;
+		$wallfx_params['count']++;
+
+		$settings_file_name = !empty($wallfx_attributes[2]) ? $wallfx_attributes[2] : 'settings.xml';
+		$settings_file_path = WP_PLUGIN_DIR."/{$plugin_dir}/component/$settings_file_name";
+
+		if (file_exists($settings_file_path)) {
+			$data = simplexml_load_file($settings_file_path);
+			$width = (int)$data->General_Properties->widthComponent->attributes()->value;
+			$height = (int)$data->General_Properties->heightComponent->attributes()->value;
+		}
+
+		if ($width == 0 || $height == 0) {
+			return '';
+		}
+
+		$swf_embed = array(
+			'width' => $width,
+			'height' => $height,
+			'text' => trim($wallfx_attributes[3]),
+			'component_path' => WP_PLUGIN_URL."/{$plugin_dir}/component/",
+			'swf_name' => '3dwall.swf',
+		);
+		$swf_embed['swf_path'] = $swf_embed['component_path'].$swf_embed['swf_name'];
+
+		if (!is_feed()) {
+			$embed_code = '<div id="wall-fx'.$wallfx_params['count'].'">'.$swf_embed['text'].'</div>';
+			$embed_code .= '<script type="text/javascript">';
+			$embed_code .= "swfobject.embedSWF('{$swf_embed['swf_path']}', 'wall-fx{$wallfx_params['count']}', '{$swf_embed['width']}', '{$swf_embed['height']}', '9.0.0.0', '', { folderPath: '{$swf_embed['component_path']}'".($settings_file_name != 'settings.xml' ? ", settingsXML: '{$settings_file_name}'" : '')." }, { scale: 'noscale', salign: 'tl', wmode: 'transparent', allowScriptAccess: 'sameDomain', allowFullScreen: true }, {});";
+			$embed_code.= '</script>';
+		} else {
+			$embed_code = '<object width="'.$swf_embed['width'].'" height="'.$swf_embed['height'].'">';
+			$embed_code .= '<param name="movie" value="'.$swf_embed['swf_path'].'"></param>';
+			$embed_code .= '<param name="scale" value="noscale"></param>';
+			$embed_code .= '<param name="salign" value="tl"></param>';
+			$embed_code .= '<param name="wmode" value="transparent"></param>';
+			$embed_code .= '<param name="allowScriptAccess" value="sameDomain"></param>';
+			$embed_code .= '<param name="allowFullScreen" value="true"></param>';
+			$embed_code .= '<param name="sameDomain" value="true"></param>';
+			$embed_code .= '<param name="flashvars" value="folderPath='.$swf_embed['component_path'].($settings_file_name != 'settings.xml' ? '&settingsXML='.$settings_file_name : '').'"></param>';
+			$embed_code .= '<embed type="application/x-shockwave-flash" width="'.$swf_embed['width'].'" height="'.$swf_embed['height'].'" src="'.$swf_embed['swf_path'].'" scale="noscale" salign="tl" wmode="transparent" allowScriptAccess="sameDomain" allowFullScreen="true" flashvars="folderPath='.$swf_embed['component_path'].($settings_file_name != 'settings.xml' ? '&settingsXML='.$settings_file_name : '').'"';
+			$embed_code .= '></embed>';
+			$embed_code .= '</object>';
+		}
+
+		return $embed_code;
+	}
+
+	function wallfx_filter_content($content) {
+		return preg_replace_callback('|\[3d-wall-fx\s*(settings="([^\]]+)")?\s*\](.*)\[/3d-wall-fx\]|i', 'wallfx_get_embed_code', $content);
+	}
+
+	function wallfx_echo_embed_code($settings_xml_path = '', $div_text = '') {
+		echo wallfx_get_embed_code(array(2 => $settings_xml_path, 3 => $div_text));
+	}
+
+	function wallfx_load_swfobject_lib() {
+		wp_enqueue_script('swfobject');
+	}
+
+	add_filter('the_content', 'wallfx_filter_content');
+	add_action('init', 'wallfx_load_swfobject_lib');
+
+?>
---
+++ readme.txt
@@ -0,0 +1,36 @@
+=== 3D Wall FX ===
+Contributors: flashxml
+Tags: images, photos, widget, post, plugin, posts, sidebar, free, flash, as3, wall, 3d, gallery, thumb, thumbs, zoom, columns, rows, effect, tootip, flat, xml, rotate
+Requires at least: 2.8.0
+Tested up to: 2.9.2
+Stable tag: trunk
+
+== Description ==
+
+One of the most advanced 3D Wall on the web. Completely XML customizable, without using Flash. And it's free!
+
+= Main features =
+
+You can integrate it in any website for free without even using Flash. Customizable width and height of the overall wall, up to 1680 x 1050 pixels. You can customize the number of columns and lines, items spacing, speed, radius and also the perspective and camera distance from wall. You can have different types of wall: flat, outside 
+
+== Installation ==
+
+Make sure your Wordpress version is equal or greater than 2.8 and your hosting provider is using PHP5.
+
+1. Upload the `3d-wall-fx` directory along with all its files to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. In the post editor use the following tag to embed the 3D Wall: `[3d-wall-fx][/3d-wall-fx]`. Or add `<?php wallfx_echo_embed_code(); ?>` in your templates
+4. Go to [FlashXML.net](http://www.flashxml.net/ "Free Flash Components") and [customize your 3D Wall](http://www.flashxml.net/3d-wall.html "3D Wall") using the Live Demo. Generate the `settings.xml` text and use it to overwrite `3d-wall-fx/component/settings.xml`
+5. To use your own images, upload them to the `3d-wall-fx/component/images` directory and update the `3d-wall-fx/component/images.xml` file accordingly
+
+= No Flash support text =
+
+To support visitors without Adobe Flash, you can provide alternative textual content. From the post editor, add the text between `[3d-wall-fx]` and `[/3d-wall-fx]`. From the PHP files of your theme, add the text as *the first argument* of the `wallfx_echo_embed_code()` function call.
+
+= Additional settings file =
+
+To embed the 3D Wall more than once, you will need another settings file and (probably) another set of images. Let's assume your new file is called **settings2.xml**. From the post editor, use the following code: `[3d-wall-fx settings="settings2.xml"][/3d-wall-fx]`. From the PHP files of your theme, add the file name as *the second argument* of the `wallfx_echo_embed_code()` function call. If you use a separate set of images, don't forget to create a new XML file for that and update the `imagesXML` value in the settings file.
+
+== Screenshots ==
+
+1. The Live Demo on [FlashXML.net](http://www.flashxml.net/3d-wall.html "3D Wall") is the utility that helps easily customize your 3D Wall to fit all of your needs.
