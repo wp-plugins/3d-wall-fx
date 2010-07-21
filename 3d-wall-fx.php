077be5bad1bd18e2601a7ced0075b6e2a1a20a89
flashxml <flashxml@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-21 09:45:34 +0000

    git-svn-id: https://plugins.svn.wordpress.org/3d-wall-fx/trunk@265076 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- 3d-wall-fx.php
+++ 3d-wall-fx.php
@@ -3,7 +3,7 @@
 Plugin Name: 3D Wall FX
 Plugin URI: http://www.flashxml.net/3d-wall.html
 Description: One of the most advanced 3D Wall on the web. Completely XML customizable, without using Flash. And it's free!
-Version: 0.2.0
+Version: 0.2.1
 Author: FlashXML.net
 Author URI: http://www.flashxml.net/
 License: GPL2
