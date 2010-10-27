37cb9c0504d74fbdde762ef187a9fe920a468c2f
flashxml <flashxml@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-27 12:27:32 +0000

    git-svn-id: https://plugins.svn.wordpress.org/3d-wall-fx/trunk@304909 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- 3d-wall-fx.php
+++ 3d-wall-fx.php
@@ -79,7 +79,7 @@ License: GPL2
 	}
 
 	function wallfx_echo_embed_code($settings_xml_path = '', $div_text = '', $width = 0, $height = 0) {
-		echo wallfx_get_embed_code(array(2 => $settings_xml_path, 3 => $div_text, 4 => $width, 6 => $height));
+		echo wallfx_get_embed_code(array(2 => $settings_xml_path, 7 => $div_text, 4 => $width, 6 => $height));
 	}
 
 	function wallfx_load_swfobject_lib() {
